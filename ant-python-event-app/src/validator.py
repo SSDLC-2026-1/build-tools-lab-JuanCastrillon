43a8fde0756dab576a5997fd461324f67360d242
NosoyJuannnn Castrillon <137562803+JCast-Elinxd@users.noreply.github.com>
2026-03-10 22:43:06 +0000

    Ant Lab v1

--- ant-python-event-app/src/validator.py
+++ ant-python-event-app/src/validator.py
@@ -6,6 +6,10 @@ def is_valid_email(email: str) -> bool:
     pattern = r"^[^@\s]+@[^@\s]+\.[^@\s]+$"
     return re.match(pattern, email) is not None
 
+def is_valid_id(id:str) -> bool:
+    pattern = r"^EV\-[\d]{4}$"
+    return re.match(pattern, id) is not None
+
 def validate_attendee(attendee: dict) -> list:
     errors = []
 
@@ -21,5 +25,8 @@ def validate_attendee(attendee: dict) -> list:
 
     if attendee.get("ticket_type") not in VALID_TICKETS:
         errors.append("Invalid ticket type")
+    
+    if not is_valid_id(attendee.get("registration_code","")):
+        errors.append("Invalid registration code")
 
     return errors
